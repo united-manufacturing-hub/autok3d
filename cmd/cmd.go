6312aadf8aa55de810cd4f97f9c7b8fec0a3c24b
Ferdinand Linnenberg <fl@umh.app>
2022-11-07 13:03:42 +0100

    feat: initial release

---
+++ cmd/cmd.go
@@ -0,0 +1,8 @@
+package main
+
+import "os/exec"
+
+func CommandExists(cmd string) bool {
+	_, err := exec.LookPath(cmd)
+	return err == nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module autok3d
+
+go 1.19
+
+require (
+	github.com/akamensky/argparse v1.4.0
+	golang.org/x/sys v0.1.0
+)
